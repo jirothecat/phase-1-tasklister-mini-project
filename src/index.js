ec301a41a16b86ea1146509a27b3903494224690
Dylan Tin <dylant711@gmail.com>
2024-08-29 15:38:26 -0400

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+function handleSubmit(event){
+  event.preventDefault()
+
+  const newTaskDescription = document.getElementById('new-task-description')
+
+  const tasksList = document.getElementById('tasks')
+
+  const liElement = document.createElement('li')
+  liElement.textContent = ` ${newTaskDescription.value} `
+
+  const deleteButton = document.createElement('button')
+  deleteButton.textContent = "X"
+  deleteButton.setAttribute('data-description', newTaskDescription.value)
+  deleteButton.addEventListener("click", deleteTaskLIElement)
+  
+  liElement.appendChild(deleteButton)
+  tasksList.appendChild(liElement)
+
+  event.target.reset()
+}
+
+function deleteTaskLIElement(event){
+  event.target.parentNode.remove()
+}
+
+document.addEventListener("DOMContentLoaded", () => {  
+  const createTaskForm = document.getElementById('create-task-form')
+  createTaskForm.addEventListener('submit', handleSubmit)
 });
